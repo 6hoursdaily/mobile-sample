c91ab292776ed697addf6ebcc27d02e9751b7e69
Jerry Pascua <jerrypascua@gmail.com>
2012-11-22 12:36:06 +0800

    initial commit

---
+++ app/controllers/HomeController.rb
@@ -0,0 +1,13 @@
+class HomeController < UIViewController
+  def viewDidLoad
+    super
+
+    self.view.backgroundColor = UIColor.whiteColor
+
+    @label = UILabel.alloc.initWithFrame(CGRectZero)
+    @label.text = "How to Create the Juice"
+    @label.sizeToFit
+    @label.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2)
+    self.view.addSubview @label
+  end
+end
